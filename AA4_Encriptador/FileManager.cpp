54c98353b19bb167dbc2992f4cea7b74b2478458
Sejusa <sergifunes07@gmail.com>
2025-12-26 12:31:44 +0100

    Ask to recover (unfinished), read file (unfinished), input control (finished).

--- AA4_Encriptador/FileManager.cpp
+++ AA4_Encriptador/FileManager.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "InputControl.h"
 
 void writeOnFile() {
 	std::cout << "Write all the messages you want. If you want to stop, please write \"exit\"." << std::endl;
@@ -8,7 +9,7 @@ void writeOnFile() {
 	std::ofstream file; //To write on the archive.
 	file.open("chat.txt", std::ios::app);
 
-	if (file.is_open()) {
+	if (file.is_open()) { //I check again to avoid crashes if the user has deleted the archive.
 		std::string line;
 
 		while (line != "exit") {
@@ -18,13 +19,40 @@ void writeOnFile() {
 				file << line << std::endl; //We write on the archive.
 			}
 		}
+
+		std::cout << "Saving data. Leaving the program..." << std::endl;
 	}
 
 	else {
 		std::cerr << "We couldn't open your archive." << std::endl;
 	}
 
-	std::cout << "Saving data. Leaving the program..." << std::endl;
-
 	file.close();
+}
+
+void askToRecover() {
+	std::ifstream file;
+	file.open("chat.txt");
+
+	if (file.is_open()) { //To check if the archive has been created or already exists.
+		file.close();
+
+		std::cout << "Old messages saved detected. Do you want to recover them? \"y\" to recover, anything to reject:" << std::endl;
+
+		std::string input;
+		inputControl(&input); //It will return a pointer an change the value of input.
+		
+		if (input == "y") {
+			
+		}
+
+		else {
+			
+		}
+	}
+
+	else {
+		std::cout << "There are no saved messages, starting a new conversation." << std::endl;
+		writeOnFile();
+	}
 }
